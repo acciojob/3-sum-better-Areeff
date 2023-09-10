40d5b2643156662f5e3564eaa17167e4b271ad6e
Areeff <120462463+Areeff@users.noreply.github.com>
2023-09-10 14:53:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
-	int n=arr.length;
+	let n=arr.length;
     arr.sort((a,b) =>a-b);
-	int closestSum=1000000000;
+	let closestSum=1000000000;
 
-	for(int i=0;i<n-2;i++){
-		int j=i+1;
-		int k=n-1;
+	for(let i=0;i<n-2;i++){
+		let j=i+1;
+		let k=n-1;
 		while(j<k){
-			int sum=arr[i]+arr[j]+arr[k];
+			let sum=arr[i]+arr[j]+arr[k];
 			if(Math.abs(target-closestsum)>(target-sum)){
 				 closestsum=sum;
 			}
